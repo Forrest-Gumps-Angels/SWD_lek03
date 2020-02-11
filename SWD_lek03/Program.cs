af6b678b415853ecccf0eb7fc7af60bba693ed4d
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-02-11 11:25:06 +0100

    Add project files.

---
+++ SWD_lek03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWD_lek03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
